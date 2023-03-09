efdee98b5ededcfbae05a0e60b0aba2d43b38260
MQuitr <musienko.0613@inbox.ru>
2023-03-09 20:13:00 +0500

    Create Database Schema

---
+++ app/src/main/java/ru/examplemquit/electrocook/data/Recipe.kt
@@ -0,0 +1,15 @@
+package ru.examplemquit.electrocook.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipe_table")
+data class Recipe (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val description: String,
+    val ingredient: String,
+    val steps: String,
+    val pathImage: String
+    )
