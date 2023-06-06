108bf62cd82a84f63bc520b26c33edb684566149
maksim <musienko.0613@inbox.ru>
2023-06-07 00:13:02 +0500

    Addition feedback intent in main menu button

--- app/src/main/java/ru/examplemquit/electrocook/StartActivity.kt
+++ app/src/main/java/ru/examplemquit/electrocook/StartActivity.kt
@@ -1,15 +1,20 @@
 package ru.examplemquit.electrocook
 
+import android.annotation.SuppressLint
 import android.app.UiModeManager
+import android.content.Intent
 import android.content.res.Configuration
 import android.content.res.Resources.Theme
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
@@ -20,10 +25,13 @@ import ru.examplemquit.electrocook.fragments.recipe.FavoriteFragmentDirections
 import ru.examplemquit.electrocook.fragments.recipe.RecipeFragmentDirections
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import kotlin.math.log
 
 class StartActivity : AppCompatActivity() {
 
     private var isDarkTheme: Boolean = true
+    private val mailSupport: String = "musienko.0613@inbox.ru"
+    private val themeMail: String = "Предложение/Баг"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         when (resources.configuration.uiMode.and(Configuration.UI_MODE_NIGHT_MASK)) {
@@ -37,67 +45,11 @@ class StartActivity : AppCompatActivity() {
         return true
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.fragment_main)
         return when (item.itemId) {
 
-           R.id.action_changeTheme -> {
-               isDarkTheme = when (isDarkTheme) {
-                   true -> {
-                       AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                       false
-                   }
-
-                   false -> {
-                       AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                       true
-                   }
-               }
-               true
-           }
-
-           R.id.action_about -> {
-               when (navController.currentDestination?.id) {
-                   R.id.listFragment -> {
-                       navController.navigate(R.id.action_listFragment_to_aboutFragment)
-                   }
-                   R.id.recipeFragment -> {
-                       navController.navigate(R.id.action_recipeFragment_to_aboutFragment)
-                   }
-                   R.id.favoriteFragment -> {
-                       navController.navigate(R.id.action_favoriteFragment_to_aboutFragment)
-                   }
-               }
-               true
-           }
-
-           R.id.action_orderCompany -> {
-                when (navController.currentDestination?.id) {
-                    R.id.listFragment -> {
-                        navController.navigate(R.id.action_listFragment_to_orderComFragment)
-                    }
-                    R.id.recipeFragment -> {
-                        navController.navigate(R.id.action_recipeFragment_to_orderComFragment)
-                    }
-                    R.id.favoriteFragment -> {
-                        navController.navigate(R.id.action_favoriteFragment_to_orderComFragment)
-                    }
-                }
-               true
-           }
-
-            R.id.action_favorite -> {
-                // переход на окно с избранными рецептами
-                when (navController.currentDestination?.id) {
-                    R.id.listFragment -> {
-                        navController.navigate(R.id.action_listFragment_to_favoriteFragment)
-                    }
-                    R.id.recipeFragment -> {
-                        navController.navigate(R.id.action_recipeFragment_to_favoriteFragment)
-                    }
-                }
-                true
-            }
             R.id.action_randomRecipe -> {
                 val viewModel = ViewModelProviders.of(this).get(RecipeViewModel::class.java)
                 viewModel.fetchRandomRecipe()
@@ -120,6 +72,85 @@ class StartActivity : AppCompatActivity() {
                 })
                 true
             }
+
+            R.id.action_favorite -> {
+                // переход на окно с избранными рецептами
+                when (navController.currentDestination?.id) {
+                    R.id.listFragment -> {
+                        navController.navigate(R.id.action_listFragment_to_favoriteFragment)
+                    }
+                    R.id.recipeFragment -> {
+                        navController.navigate(R.id.action_recipeFragment_to_favoriteFragment)
+                    }
+                }
+                true
+            }
+
+            R.id.action_orderCompany -> {
+                when (navController.currentDestination?.id) {
+                    R.id.listFragment -> {
+                        navController.navigate(R.id.action_listFragment_to_orderComFragment)
+                    }
+                    R.id.recipeFragment -> {
+                        navController.navigate(R.id.action_recipeFragment_to_orderComFragment)
+                    }
+                    R.id.favoriteFragment -> {
+                        navController.navigate(R.id.action_favoriteFragment_to_orderComFragment)
+                    }
+                }
+                true
+            }
+
+           R.id.action_changeTheme -> {
+               isDarkTheme = when (isDarkTheme) {
+                   true -> {
+                       AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                       false
+                   }
+
+                   false -> {
+                       AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                       true
+                   }
+               }
+               true
+           }
+
+           R.id.action_feedBack -> {
+               val email = mailSupport
+               val subject = themeMail
+
+               val intent = Intent(Intent.ACTION_SEND).apply {
+                   type = "text/plain"
+                   putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                   putExtra(Intent.EXTRA_SUBJECT, subject)
+               }
+
+               if (intent.resolveActivity(packageManager) != null) {
+                   val chooser = Intent.createChooser(intent, "Выберите приложение для отправки письма")
+                   startActivity(chooser)
+               } else {
+                   Toast.makeText(applicationContext, "Нету подходящего приложения для отправки письма", Toast.LENGTH_SHORT).show()
+               }
+
+               true
+           }
+
+           R.id.action_about -> {
+               when (navController.currentDestination?.id) {
+                   R.id.listFragment -> {
+                       navController.navigate(R.id.action_listFragment_to_aboutFragment)
+                   }
+                   R.id.recipeFragment -> {
+                       navController.navigate(R.id.action_recipeFragment_to_aboutFragment)
+                   }
+                   R.id.favoriteFragment -> {
+                       navController.navigate(R.id.action_favoriteFragment_to_aboutFragment)
+                   }
+               }
+               true
+           }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
